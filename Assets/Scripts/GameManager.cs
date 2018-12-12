46b6a0adfcd2c334064f140cc80dee0149f5aa43
Heejo Kang <comforter1809@gmail.com>
2018-12-12 13:03:04 +0900

    포톤 연결 오류상태로 일단 끝

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public class GameManager : PunBehaviour {
+
+    public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        if (playerPrefab)
+        {
+            if (Player.LocalPlayerInstance == null)
+            {
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0, 0, 0), Quaternion.identity, 0);
+            }
+
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
